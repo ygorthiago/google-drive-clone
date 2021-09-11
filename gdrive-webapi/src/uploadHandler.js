b37f1692821a9f63f6037d0dcabbb8f2039f10dc
ygorthiago <ygorthiagoff@outlook.com>
2021-09-11 17:45:52 -0300

    fix:  canExecute validation

--- gdrive-webapi/src/uploadHandler.js
+++ gdrive-webapi/src/uploadHandler.js
@@ -3,17 +3,17 @@ import { pipeline } from 'stream/promises'
 import fs from 'fs'
 import { logger } from './logger'
 
-export default class Uploadhandler {
-  constructor({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
+export default class UploadHandler {
+  constructor({ io, socketId, downloadsFolder, messageTimerDelay = 200 }) {
     this.io = io,
     this.socketId = socketId,
     this.downloadsFolder = downloadsFolder,
-    this.messageTimeDelay = messageTimeDelay,
+    this.messageTimerDelay = messageTimerDelay,
     this.ON_UPLOAD_EVENT = 'file-upload'
   }
 
   canExecute(lastExecution) {
-    return (Date.now() -lastExecution) > this.messageTimeDelay
+    return (Date.now() - lastExecution) >= this.messageTimerDelay
   }
 
   handleFileBytes(filename) {
@@ -21,19 +21,21 @@ export default class Uploadhandler {
     
     async function* handleData(source) {
       let processedAlready = 0
+
       for await (const chunk of source) {
         yield chunk
 
         processedAlready += chunk.length
 
-        if (!this.canExecute()) {
+        if (!this.canExecute(this.lastMessageSent)) {
           continue;
         }
+
+        this.lastMessageSent = Date.now()
 
         this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, { processedAlready, filename })
         logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`)
       }
-
     }
 
     return handleData.bind(this)
@@ -46,7 +48,7 @@ export default class Uploadhandler {
       //get readable stream
       file,
       // filter, convert, transform data
-      this.handleFileBytes.apply(this, [ filename ]),
+      this.handleFileBytes.apply(this, [filename]),
       // proccess output, a writable stream
       fs.createWriteStream(saveTo)
     )

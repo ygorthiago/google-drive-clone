d0866849cb59d2d207e1c39edb9b0b6627113d35
ygorthiago <ygorthiagoff@outlook.com>
2021-09-12 12:10:04 -0300

    tests: routes integration test

---
+++ gdrive-webapi/test/integration/routes.test.js
@@ -0,0 +1,69 @@
+import { describe, test, jest, expect } from '@jest/globals'
+import fs from 'fs'
+import FormData from 'form-data'
+import TestUtil from '../_util/testUtil.js'
+import { logger } from '../../src/logger'
+import Routes from './../../src/routes.js'
+import { tmpdir } from 'os'
+import { join } from 'path'
+
+describe('#Routes Integration Test', () => {
+  let defaultDownloadsFolder = ''
+  beforeAll(async () => {
+    defaultDownloadsFolder = await fs.promises.mkdtemp(join(tmpdir(), 'downloads-'))
+  })
+
+  afterAll(async () => {
+    await fs.promises.rm(defaultDownloadsFolder, { recursive: true })
+  })
+
+  beforeEach(() => {
+    jest.spyOn(logger, 'info')
+      .mockImplementation()
+  })
+
+  describe('#POST', () => {
+    const ioObj = {
+      to: (id) => ioObj,
+      emit: (event, message) => { }
+    }
+
+    test('should upload file to the folder', async () => {
+      const filename = 'gmail-icon.png'
+      const fileStream = fs.createReadStream(`./test/integration/mocks/${filename}`)
+      const response = TestUtil.generateWritableStream(() => {  })
+
+      const form = new FormData()
+      form.append('photo', fileStream)
+
+      const defaultParams = {
+        request: Object.assign(form, {
+          headers: form.getHeaders(),
+          method: 'POST',
+          url: '?socketId=10'
+        }),
+        response: Object.assign(response, {
+          setHeader: jest.fn(),
+          writeHead: jest.fn(),
+          end: jest.fn(),
+        }),
+        values: () => Object.values(defaultParams)
+      }
+
+      const routes = new Routes(defaultDownloadsFolder)
+      routes.setSocketInstance(ioObj)
+
+      const dirBeforeRan = await fs.promises.readdir(defaultDownloadsFolder)
+      expect(dirBeforeRan).toEqual([])
+      
+      await routes.handler(...defaultParams.values())
+
+      const dirAfterRan = await fs.promises.readdir(defaultDownloadsFolder)
+      expect(dirAfterRan).toEqual([filename])
+
+      expect(defaultParams.response.writeHead).toHaveBeenCalledWith(200)
+      const expectedResult = JSON.stringify({ result: 'Files uploaded with success!' })
+      expect(defaultParams.response.end).toHaveBeenCalledWith(expectedResult)   
+    })
+  })
+})
